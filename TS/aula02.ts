0200e4e0000f588e1828c2c133f13316d8bdac6c
Matheus Fernandes <matheus_fer200@hotmail.com>
2024-02-16 17:33:11 -0300

    Repositorio criado

---
+++ TS/aula02.ts
@@ -0,0 +1,28 @@
+//adicionando a variavel e o id dele
+const formulario = document.getElementById('formulario') as HTMLFormElement
+//recebendo os dados do formulario
+
+formulario.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const nome = (document.getElementById('nome') as HTMLInputElement).value
+    const telefone = (document.getElementById('telefone') as HTMLInputElement).value
+    const dataNascimento = (document.getElementById('dataNascimento') as HTMLInputElement).value
+    const email = (document.getElementById('email') as HTMLInputElement).value
+    const cpf = (document.getElementById('cpf') as HTMLInputElement).value
+
+    const dados = `
+Nome: ${nome} <br>
+Telefone: ${telefone} <br>
+CPF: ${cpf} <br>
+Data de Nascimento: ${dataNascimento} <br>
+E-mail: ${email}`
+
+    const exibir = document.getElementById('resposta')
+    if (exibir) {
+        exibir.innerHTML = dados
+    }
+
+
+
+
+})
