b00e534152ad11f9cc3400250a3c71a4c38e8a3b
Matheus Fernandes <matheusfer200@gmail.com>
2024-02-20 23:21:33 -0300

    Arquivos JS transpilados

--- dist/ex03.js
+++ dist/ex03.js
@@ -1,56 +1,38 @@
 "use strict";
+//criando a classe conta
 class Conta {
-    saldo;
-    tipoConta;
     titular;
+    saldo;
     agencia;
     contaCorrente;
     historicoTransacao;
-    constructor(saldo, tipoConta, titular, agencia, contaCorrente, historicoTransacao) {
-        this.saldo = saldo;
-        this.tipoConta = tipoConta;
+    constructor(titular, saldo, agencia, contaCorrente, historicoTransacao) {
         this.titular = titular;
+        this.saldo = saldo;
         this.agencia = agencia;
         this.contaCorrente = contaCorrente;
         this.historicoTransacao = historicoTransacao;
     }
     mostrarDados() {
-        console.log(`saldo atual: ${this.saldo}\n Tipo de conta: ${this.tipoConta}\n
+        console.log(`saldo atual: ${this.saldo}\n
         Titular: ${this.titular} \n Agência: ${this.agencia}\n Conta Corrente: ${this.contaCorrente}\n
         Histórico de transações: ${this.historicoTransacao}`);
     }
 }
-class Despeza {
-    despeza;
+class Transacao {
+    valor;
+    data;
     categoria;
-    constructor(despeza, categoria) {
-        this.despeza = despeza;
+    tipoTransacao;
+    constructor(valor, data, tipoTransacao, categoria) {
+        this.valor = valor;
+        this.data = data;
+        this.tipoTransacao = tipoTransacao; // receita ou despesa
         this.categoria = categoria;
     }
     mostrarDados() {
-        console.log(`Despezas: ${this.despeza}\n Categoria: ${this.categoria}`);
-    }
-}
-class Receita {
-    receita;
-    categoria;
-    constructor(receita, categoria) {
-        this.receita = receita;
-        this.categoria = categoria;
-    }
-    mostrarDados() {
-        console.log(`Receitas: ${this.receita}\n Categoria: ${this.categoria}`);
-    }
-}
-class RelatorioMensal {
-    gastos;
-    ganhos;
-    constructor(gastos, ganhos) {
-        this.ganhos = ganhos;
-        this.gastos = gastos;
-    }
-    mostrarDados() {
-        console.log(`Ganhos do mês: ${this.ganhos}\n Gastos do mês: ${this.gastos} }`);
+        console.log(`Despesas: ${this.valor}\n Data: ${this.data}\n Tipo de transação: 
+        ${this.tipoTransacao}\n Categoria: ${this.categoria}`);
     }
 }
 class Planejamento {
@@ -58,5 +40,27 @@ class Planejamento {
     valor;
     periodo;
     constructor(objetivo, valor, periodo) {
+        this.objetivo = objetivo;
+        this.valor = valor;
+        this.periodo = periodo;
+    }
+    mostrarDados() {
+        console.log(`O dinheiro será poupado para ${this.objetivo} \n
+        Você deverá guardar ${this.valor} reais por mês até ${this.periodo} para atingir sua meta`);
     }
 }
+//inicializando
+//criando a primeira conta
+const conta1 = new Conta('Matheus Fernandes', 910, '321', '10203040', []);
+//criando a primeira transacao
+const transacao1 = new Transacao(1000, new Date('2024-2-10'), 'Receita', 'Pix recebido');
+//inserindo a transacao no historico
+conta1.historicoTransacao.push(transacao1);
+const transacao2 = new Transacao(90, new Date('2024-2-12'), 'Despesa', 'Pagamento Boleto');
+//inserindo a transacao no historico
+conta1.historicoTransacao.push(transacao2);
+//criando a primeira meta
+//exibir os dados
+console.log(conta1.mostrarDados());
+console.log(transacao1.mostrarDados());
+console.log(transacao2.mostrarDados());
---
+++ dist/ex04.js
@@ -0,0 +1,72 @@
+"use strict";
+class Etapas {
+    nome;
+    prazo;
+    status;
+    constructor(nome, prazo, status) {
+        this.nome = nome;
+        this.prazo = prazo;
+        this.status = status;
+    }
+    mostrarDadosEtapa() {
+        console.log(`Etapa: ${this.nome}\n Prazo final: ${this.prazo}\n Status da etapa: ${this.status}`);
+    }
+}
+class Tarefa {
+    nome;
+    responsavel;
+    prazo;
+    status;
+    descricao;
+    constructor(nome, responsavel, prazo, status, descricao) {
+        this.nome = nome;
+        this.responsavel = responsavel;
+        this.prazo = prazo;
+        this.status = status;
+        this.descricao = descricao;
+    }
+    mostrarDadosTarefa() {
+        console.log(`Nome da tarefa: ${this.nome}\n Responsável: ${this.responsavel}\n Prazo: ${this.prazo}\n
+    Status: ${this.status}\n Descrição: ${this.descricao}`);
+    }
+}
+class MembroEquipe {
+    nome;
+    email;
+    cargo;
+    constructor(nome, email, cargo) {
+        this.nome = nome;
+        this.email = email;
+        this.cargo = cargo;
+    }
+    mostrarDadosMembro() {
+        console.log(`Membro: ${this.nome}\n E-mail: ${this.email}\n Cargo: ${this.cargo}`);
+    }
+}
+class Projeto {
+    nome;
+    descricao;
+    equipe;
+    etapas;
+    tarefas;
+    constructor(nome, descricao, equipe, etapas, tarefas) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.equipe = equipe;
+        this.etapas = etapas;
+        this.tarefas = tarefas;
+    }
+    mostrarDadosProjeto() {
+        console.log(`Nome do projeto: ${this.nome}\n Descricao: ${this.descricao}\n
+        Equipe Responsável: ${this.equipe}\n Etapas: ${this.etapas}\n Tarefas: ${this.tarefas}`);
+    }
+}
+//inicializando
+const projeto1 = new Projeto('Sistema gerenciamento de projetos', 'Sistema criado para acompanhar o andamentos das atividades', [], [], []);
+const etapa1 = new Etapas('Algoritmo', new Date(2024 - 3 - 1), 'Em andamento');
+const tarefa1 = new Tarefa('Escrever pseudocodigo', 'Valtemir', new Date('2024-02-28'), 'Em andamento', 'Descrever a sequencia de passos lógicos para implementar o projeto');
+const membro1 = new MembroEquipe('Valtemir', 'vlimap@gmail.com', 'Chefe');
+projeto1.equipe.push(membro1.nome);
+projeto1.etapas.push(etapa1);
+projeto1.tarefas.push(tarefa1);
+console.log(projeto1.mostrarDadosProjeto());
